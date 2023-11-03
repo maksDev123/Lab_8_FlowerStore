e84cd8b60ced25bd1a03eae9133fe00c7836b70d
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2023-11-03 23:00:09 +0200

    Changed tests

--- src/test/java/ua/edu/apps/lab71/Lab71ApplicationTests.java
+++ src/test/java/ua/edu/apps/lab71/Lab71ApplicationTests.java
@@ -1,13 +1,13 @@
-package ua.edu.apps.lab71;
+// package ua.edu.apps.lab71;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Lab71ApplicationTests {
+// @SpringBootTest
+// class Lab71ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
